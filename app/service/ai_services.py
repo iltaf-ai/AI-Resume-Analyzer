cb56e6f8cbcadc4ff1dd2ca7bdf951b65d2f2a52
iltafshahni-spe <iltafshahni@gmail.com>
2026-09-05 15:27:10 +0500

    Register UI Setup

--- app/service/ai_services.py
+++ app/service/ai_services.py
@@ -1,2 +1,38 @@
 from langchain_mistralai import ChatMistralAI
 from langchain_core.prompts import ChatPromptTemplate
+from pydantic import BaseModel
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatMistralAI(
+    model = "small-mistral-latest"
+
+)
+
+prompt = ChatPromptTemplate([
+    ("system", 
+    
+    """Summarizes the Resume
+    - Road Map Explantion
+    - """),
+
+    ("human",
+    """
+    {resume}
+    """
+    )
+])
+
+def resume_analyzer(resume):
+    message = prompt.invoke({
+        "reusme": resume
+    })
+
+    result = model.invoke(message)
+
+    return result.content
+
+
+
+
