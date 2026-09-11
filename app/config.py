054e1ceb1306e156ed9771b9d5a3d06c70fd369e
iltafshahni-spe <iltafshahni@gmail.com>
2026-09-11 10:32:32 +0500

    fix config for deployment

--- app/config.py
+++ app/config.py
@@ -5,9 +5,7 @@ load_dotenv()
 class settings():
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     SECRET_KEY  = os.getenv("SECRET_KEY")
-    ACCESS_EXPIRE_TOKEN_TIME = int(
-    os.getenv("ACCESS_EXPIRE_TOKEN_TIME")
-        )
+    ACCESS_EXPIRE_TOKEN_TIME = int(os.getenv("ACCESS_EXPIRE_TOKEN_TIME", "30"))
     DATABASE_URL = os.getenv("DATABASE_URL")
     ALGORITHM = os.getenv("ALGORITHM")
 
